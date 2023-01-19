41ae6cd8cdcda217d7c1958865ad7a8a141cc9ad
opi6opi6 <opi6@hanmail.net>
2023-01-20 01:49:42 +0900

    first

--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,11 @@ const routes = [
     name: 'home',
     component: () => import('@/views/HomeView.vue')
   },
+  {
+    path: '/temp',
+    name: 'temp',
+    component: () => import('@/views/TempView.vue')
+  },
 ]
 
 const router = new VueRouter({
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,16 +8,23 @@
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
     data() {
         return {
-            conn: undefined
+            conn: undefined,
+            room_list: []
         }
     },
     created() {
         // 서버와 웹소켓 연결을 시도해요
         this.conn = new WebSocket('ws://localhost:9998/socket');
         console.log("웹소켓 연결을 시도 했어요. 앞이 에러가 있다면 안된거에요");
+
+        this.conn.onopen = function() {
+            axios.post("http://localhost:9998/here", "lobby", {headers: {"Content-Type": "text/plain"}});
+        }
 
         // 웹소켓 서버에서 메세지가 오면 실행되는 이벤트 핸들러에요 도착하는 메세지는 msg에 string 형식으로 담겨있어요
         // msg를 json으로 바꿔서 활용할게요
@@ -29,6 +36,7 @@ export default {
     methods: {
         tempButton() {
             alert("뭔가 실험할려고 만드는 버튼이에요");
+            this.conn.close();
         },
         send(message) {
             this.conn.send(JSON.stringify(message));
